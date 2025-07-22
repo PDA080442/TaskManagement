ef4258447c25429adc511e6797cfd9cb119e02ed
PDA080442 <pda080442@mail.ru>
2025-07-22 15:16:29 +0700

    complete login form

--- src/composables/auth.request.ts
+++ src/composables/auth.request.ts
@@ -1,8 +1,19 @@
-import type { RegData } from "@/types/auth.type";
+import type { RegData, LogData, AuthTokens } from "@/types/auth.type";
 import { useApi } from "./useApi";
+import axios from "axios";
 
 const { call } = useApi('/profile')
 
 export async function regdata(formdata: RegData): Promise<void> {
   await call('/signup/', formdata, 'POST')
+}
+
+export async function logdata(logindata: LogData): Promise<AuthTokens> {
+  const tokens = await call('/signin/', logindata, 'POST') as AuthTokens
+  localStorage.setItem('accessToken', tokens.access)
+  localStorage.setItem('refreshToken', tokens.refresh)
+
+  axios.defaults.headers.common['Authorization'] = `Bearer ${tokens.access}`
+
+  return tokens as AuthTokens
 }
