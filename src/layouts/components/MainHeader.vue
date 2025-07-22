a584b8f427ad9e57941f396e7c477260fec54d1b
Artem <skoro.artem004@gmail.com>
2025-07-22 21:35:08 +0700

    feat: add header and sidebar

---
+++ src/layouts/components/MainHeader.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app-bar app fixed color="white" elevate-on-scroll flat class="border-bottom">
+    <v-container class="pa-0 d-flex justify-center" style="flex: 1 1 auto; max-width: 500px">
+      <v-text-field
+        v-model="search"
+        placeholder="Search anything..."
+        density="comfortable"
+        hide-details
+        rounded="lg"
+        clearable
+        variant="solo"
+        append-inner-icon="mdi-magnify"
+        class="header-search"
+      />
+    </v-container>
+
+    <v-btn icon class="mr-7">
+      <v-icon size="24">mdi-bell-outline</v-icon>
+      <v-badge
+        content="2"
+        color="deep-purple accent-4"
+        overlap
+        style="transform: translate(8px, -8px)"
+      />
+    </v-btn>
+
+    <v-menu offset-y bottom>
+      <template #activator="{ props }">
+        <v-btn icon v-bind="props" class="mr-10">
+          <v-avatar size="32">
+            <img src="" alt="User" />
+          </v-avatar>
+          <v-icon class="ml-3">mdi-chevron-down</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item to="/profile" router>
+          <v-list-item-title>Profile</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="logout">
+          <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const search = ref('')
+const router = useRouter()
+
+function logout() {
+  router.push('/login')
+}
+</script>
+
+<style scoped>
+.header-search:global(
+    .v-field.v-field--appended.v-field--center-affix.v-field--no-label.v-field--variant-solo.v-theme--light.rounded-lg.v-locale--is-ltr
+  ) {
+  background-color: #f3f7fa;
+  box-shadow: none;
+  color: #23235f;
+}
+
+.v-application .v-app-bar {
+  left: 72px !important;
+  width: calc(100% - 72px) !important;
+}
+</style>
---
+++ src/layouts/components/MainSidebar.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-navigation-drawer
+    app
+    fixed
+    permanent
+    width="72"
+    class="pa-0 d-flex flex-column align-center"
+    color="#FBFAFF"
+  >
+    <v-list nav dense class="flex-grow-1">
+      <v-img :src="logoIcon" alt="Logo" width="50" height="50" class="logo" />
+
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="item.title"
+        :to="item.route"
+        router
+        link
+        class="justify-center my-2 sidebar-item"
+        :class="{ 'sidebar-item--active': selectedIndex === i }"
+        @click="selectedIndex = i"
+      >
+        <v-list-item-icon>
+          <img
+            :src="item.icon"
+            :alt="item.title"
+            width="24"
+            height="24"
+            class="d-flex justify-center align-center ml-2"
+          />
+        </v-list-item-icon>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import dashboardIcon from '@/assets/sidebar/dashboard.svg?url'
+import timelineIcon from '@/assets/sidebar/timeline.svg?url'
+import taskIcon from '@/assets/sidebar/task.svg?url'
+import filesIcon from '@/assets/sidebar/files.svg?url'
+import settingsIcon from '@/assets/sidebar/settings.svg?url'
+import msgIcon from '@/assets/sidebar/message.svg?url'
+import logoIcon from '@/assets/sidebar/logo.svg?url'
+
+const items = [
+  { title: 'Dashboard', route: '/dashboard', icon: dashboardIcon },
+  { title: 'Timeline', route: '/timeline', icon: timelineIcon },
+  { title: 'Tasks', route: '/tasks', icon: taskIcon },
+  { title: 'Files', route: '/files', icon: filesIcon },
+  { title: 'Settings', route: '/settings', icon: settingsIcon },
+  { title: 'Messages', route: '/messages', icon: msgIcon },
+]
+
+const selectedIndex = ref(0)
+</script>
+
+<style scoped>
+.sidebar-item {
+  cursor: pointer;
+}
+.sidebar-item--active {
+  background-color: #f5f5f5;
+  border-radius: 12px;
+}
+
+.logo {
+  margin-bottom: 150px;
+}
+</style>
